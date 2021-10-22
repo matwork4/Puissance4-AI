99b52fa72c0633d1f2bda0448d9fefd450151ccc
matwork4 <matwork4@gmail.com>
2021-10-22 16:10:34 +0200

    add AI_tools.java
    Ajout de la classe AI_tools.java et déplacement des outils de l'IA numéro 2 dans cette nouvelle classe.

--- src/game_package/AI2.java
+++ src/game_package/AI2.java
@@ -10,24 +10,31 @@ public class AI2 {
 	int[][] terrain;
 
 	int hauteur,largeur;
-	//boolean AIturn=true;
-	//int nbPlays=0;
 	int choix = 0;
+	AI_tools tools;
 	
 	
+	/* Constructeur de la classe AI2
+	 * Permet d'initialiser l'IA numéro 2
+	 */
 	public AI2(Terrain T,int DIM_X, int DIM_Y) {
-		this.T=T;
-		this.terrain=copyTerrain(T.terrain);
 		
+		//On initialise les outils de l'IA
+		this.tools = new AI_tools(DIM_X,DIM_Y);
+		
+		this.terrain=tools.copyTerrain(T.terrain);
+		this.T=T;
 		this.hauteur=DIM_X;
 		this.largeur=DIM_Y;
-
 		
 	}
 	
 	
+	/* Fonction Play :
+	 * Permet à l'IA de jouer son tour
+	 */
 	public void play(Terrain terrain1) {
-		terrain=copyTerrain(terrain1.terrain);
+		terrain=tools.copyTerrain(terrain1.terrain);
 		int nbPlays=0;
 		int profondeur = 6;
 		
@@ -40,18 +47,21 @@ public class AI2 {
 	}
 	
 	
+	/* Algorithme minMax :
+	 * 
+	 */
 	int minMax(int[][] tab, int nbPlays, int profondeur) {
-		//Test si le terrain est remplis 
+		//Test si le terrain est remplit 
 		if(nbPlays==hauteur*largeur) {
 			return 0;
 		}
 		
 		//true si c'est le tour de l'IA, false sinon
-		boolean AIturn = isPair(nbPlays);
+		boolean AIturn = tools.isPair(nbPlays);
 		
 		for(int i=0;i<largeur;i++) {
-			if(colonneIsNotFull(i,tab)){
-				if(estVictorieux(i,tab,AIturn)) {
+			if(tools.colonneIsNotFull(i,tab)){
+				if(tools.estVictorieux(i,tab,AIturn)) {
 					int value = (hauteur*largeur+1 - nbPlays)/2;
 					return value;
 				}
@@ -63,10 +73,10 @@ public class AI2 {
 		
 		if(profondeur>0) {
 			for(int i=0; i<largeur;i++) {
-				if(colonneIsNotFull(i,tab)) {
+				if(tools.colonneIsNotFull(i,tab)) {
 				
 					int[][] newTab;
-					newTab=addJeton(i,tab,AIturn);
+					newTab=tools.addJeton(i,tab,AIturn);
 					int score = -minMax(newTab, nbPlays-1, profondeur-1);
 					//Il manque ça : 
 					if(i==0) {
@@ -87,151 +97,6 @@ public class AI2 {
 	}
 	
 	
-	//Ajoute un jeton au terrain
-	public int[][] addJeton(int indice_colonne, int[][] OldTab, boolean AIturn){
-		int[][] tab;
-		tab=copyTerrain(OldTab);
-		
-		int indice_ligne = -1;
-
-		for(int i=hauteur-1;i>=0;i--) {
-			if (tab[i][indice_colonne]==0){
-				
-				if(AIturn) {
-					tab[i][indice_colonne]=3;
-				}else {
-					tab[i][indice_colonne]=1;
-				}
-				indice_ligne = i;
-				i=-1;
-			}
-		}
-		
-		if(indice_ligne==-1) {
-			System.out.println("===== Erreur : addJeton indice_ligne = -1 =====");
-		}
-		
-		return tab;
-	}
-	
-	
-	public boolean isPair(int d) {
-		boolean a = (d%2 == 0);
-		//System.out.println("=============\n" + d + " est-il pair ? : "+a);
-		return a;
-	}
-	
-	
-	
-	
-	
-	public int[][] copyTerrain(int[][] terrain){
-		int[][] newTerrain;
-		newTerrain = new int[terrain.length][terrain[0].length];
-		
-		for(int i=0;i<terrain.length;i++) {
-			for(int j=0;j<terrain[i].length;j++) {
-				newTerrain[i][j]=terrain[i][j];
-			}
-		}
-		
-		return newTerrain;
-	}
-	
-	
-	//bool canPlay(int col) const;
-	/* retourne true si la colonne n'est pas pleine 
-	 * false sinon
-	 */
-	public boolean colonneIsNotFull(int indice_colonne, int[][] tab) {
-		if(tab[0][indice_colonne]==0) {
-			return true;
-		}
-		return false;
-	}
-	
-	
-	//bool isWinningMove(int col) const;
-	/* retourne true si le joueur est victorieur losqu'il joue ici
-	 * retourne false sinon
-	 */
-	public boolean estVictorieux(int indice_colonne, int[][] OldTab, boolean AIturn) {
-		int[][] tab;
-		tab=copyTerrain(OldTab);
-		
-		int indice_ligne = -1;
-		
-		for(int i=hauteur-1;i>=0;i--) {
-			if (tab[i][indice_colonne]==0){
-				
-				if(AIturn) {
-					tab[i][indice_colonne]=3;
-				}else {
-					tab[i][indice_colonne]=1;
-				}
-				indice_ligne = i;
-				i=-1;
-			}
-		}
-		
-		if(indice_ligne==-1) {
-			afficheTable(tab);
-			System.out.println("indice_colonne = "+indice_colonne);
-			System.out.println("===== Erreur : estVictorieux indice_ligne = -1 =====");
-			return false;
-		}
-		
-		
-		/* Test si le nouveau terrain obtenu est victorieux 
-		 */
-		
-		for(int i=0;i<tab.length;i++) {
-			for(int j=0;j<tab[i].length;j++) {
-				if(tab[i][j] != 0) {
-				
-					//test victoire en largeur 
-					if(j+3 < tab[i].length ) {
-						if(tab[i][j]==tab[i][j+1] && tab[i][j]==tab[i][j+2] && tab[i][j]==tab[i][j+3]) {
-							
-							return true;
-						}
-					}
-					//test victoire en hauteur
-					if(i+3 < tab.length ) {
-						if(tab[i][j]==tab[i+1][j] && tab[i][j]==tab[i+2][j] && tab[i+3][j]==tab[i][j]) {
-							
-							return true;
-						}
-					}
-					//test victoire en diagonale (de haut gauche à bas droite)
-					if(i+3 < tab.length && j+3 < tab[i].length) {
-						if(tab[i][j]==tab[i+1][j+1] && tab[i][j]==tab[i+2][j+2] && tab[i+3][j+3]==tab[i][j]) {
-							
-							return true;
-						}
-					}
-					//test victoire en diagonale (de haut droite à bas gauche)
-					if(i+3 < tab.length && j-3 >= 0) {
-						if(tab[i][j]==tab[i+1][j-1] && tab[i][j]==tab[i+2][j-2] && tab[i+3][j-3]==tab[i][j]) {
-							return true;
-						}
-					}
-				}
-			}
-		}
-			
-		return false;
-	}
-	
-	public void afficheTable(int[][] tab) {
-		System.out.println("\n------- LA TABLE --------");
-		for(int i=0;i<tab.length;i++) {
-			System.out.println();
-			for(int j=0;j<tab[i].length;j++) {
-				System.out.print(tab[i][j] + " ");
-			}
-		}
-	}
 	
 	
 }
---
+++ src/game_package/AI_tools.java
@@ -0,0 +1,192 @@
+package game_package;
+
+public class AI_tools {
+
+	int hauteur,largeur;
+	
+	/* Constructeur de la classe AI_tools
+	 * On initialise les méthodes qui seront utilisées 
+	 * par les intelligences artificielles
+	 */
+	public AI_tools(int DIM_X, int DIM_Y) {
+		
+		this.hauteur=DIM_X;
+		this.largeur=DIM_Y;	
+	}
+	
+	
+	/* ======================================
+	 * Fonctions utilisées par l'IA numéro 2
+	 * ============== VVVVVV ================
+	 */
+	
+	
+	/* Boolean isPair :
+	 * Cette fonction renvoie true si pair, false si impair
+	 */
+	public boolean isPair(int d) {
+		boolean a = (d%2 == 0);
+		//System.out.println("=============\n" + d + " est-il pair ? : "+a);
+		return a;
+	}
+	
+	
+	/* Integer copyTerrain :
+	 * Cette fonction permet de copier un terrain dans un autre
+	 * afin de ne pas ré-écrire par dessus
+	 */
+	public int[][] copyTerrain(int[][] terrain){
+		int[][] newTerrain;
+		newTerrain = new int[terrain.length][terrain[0].length];
+		
+		for(int i=0;i<terrain.length;i++) {
+			for(int j=0;j<terrain[i].length;j++) {
+				newTerrain[i][j]=terrain[i][j];
+			}
+		}
+		
+		return newTerrain;
+	}
+	
+	
+	/* Boolean colonneIsNotFull :
+	 * retourne true si la colonne n'est pas remplie et false sinon. 
+	 * Elle permet de vérifier si il reste au moins une place pour jouer un jeton.
+	 * (équivalent de la fonction [bool canPlay(int col) const;] )
+	 */
+	public boolean colonneIsNotFull(int indice_colonne, int[][] tab) {
+		if(tab[0][indice_colonne]==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	/* Boolean estVictorieux :
+	 * retourne true si le joueur est victorieux losqu'il joue à cette position et false sinon.
+	 * Cette fonction parcours le terrain pour vérifier si il y a une victoire 
+	 * de l'un des deux joueurs.
+	 * (équivalent de la fonction [bool isWinningMove(int col) const;] )
+	 */
+	public boolean estVictorieux(int indice_colonne, int[][] OldTab, boolean AIturn) {
+		int[][] tab;
+		tab=copyTerrain(OldTab);
+		
+		int indice_ligne = -1;
+		
+		for(int i=hauteur-1;i>=0;i--) {
+			if (tab[i][indice_colonne]==0){
+				
+				if(AIturn) {
+					tab[i][indice_colonne]=3;
+				}else {
+					tab[i][indice_colonne]=1;
+				}
+				indice_ligne = i;
+				i=-1;
+			}
+		}
+		
+		if(indice_ligne==-1) {
+			afficheTable(tab);
+			System.out.println("indice_colonne = "+indice_colonne);
+			System.out.println("===== Erreur : estVictorieux indice_ligne = -1 =====");
+			return false;
+		}
+		
+		//Test si le nouveau terrain obtenu est victorieux 
+		for(int i=0;i<tab.length;i++) {
+			for(int j=0;j<tab[i].length;j++) {
+				if(tab[i][j] != 0) {
+				
+					//test victoire en largeur 
+					if(j+3 < tab[i].length ) {
+						if(tab[i][j]==tab[i][j+1] && tab[i][j]==tab[i][j+2] && tab[i][j]==tab[i][j+3]) {
+							
+							return true;
+						}
+					}
+					//test victoire en hauteur
+					if(i+3 < tab.length ) {
+						if(tab[i][j]==tab[i+1][j] && tab[i][j]==tab[i+2][j] && tab[i+3][j]==tab[i][j]) {
+							
+							return true;
+						}
+					}
+					//test victoire en diagonale (de haut gauche à bas droite)
+					if(i+3 < tab.length && j+3 < tab[i].length) {
+						if(tab[i][j]==tab[i+1][j+1] && tab[i][j]==tab[i+2][j+2] && tab[i+3][j+3]==tab[i][j]) {
+							
+							return true;
+						}
+					}
+					//test victoire en diagonale (de haut droite à bas gauche)
+					if(i+3 < tab.length && j-3 >= 0) {
+						if(tab[i][j]==tab[i+1][j-1] && tab[i][j]==tab[i+2][j-2] && tab[i+3][j-3]==tab[i][j]) {
+							
+							return true;
+						}
+					}
+				}
+			}
+		}
+			
+		return false;
+	}
+	
+	
+	/* Integer[][] addJeton :
+	 * Cette fonction permet d'ajouter un jeton dans le terrain à un indice donné.
+	 * Elle ajoutera un jeton du joueur ou de l'IA en fonction du tour effectif,
+	 * puis renvoie un nouveau tableau qui représente le terrain.
+	 */
+	public int[][] addJeton(int indice_colonne, int[][] OldTab, boolean AIturn){
+		int[][] tab;
+		tab=copyTerrain(OldTab);
+		
+		int indice_ligne = -1;
+
+		for(int i=hauteur-1;i>=0;i--) {
+			if (tab[i][indice_colonne]==0){
+				
+				if(AIturn) {
+					tab[i][indice_colonne]=3;
+				}else {
+					tab[i][indice_colonne]=1;
+				}
+				indice_ligne = i;
+				i=-1;
+			}
+		}
+		
+		if(indice_ligne==-1) {
+			System.out.println("===== Erreur : addJeton indice_ligne = -1 =====");
+		}
+		
+		return tab;
+	}
+	
+	
+	/* Void afficheTable :
+	 * Cette fonction affiche le tableau qui représente le terrain dans la console.
+	 */
+	public void afficheTable(int[][] tab) {
+		System.out.println("\n------- LA TABLE --------");
+		for(int i=0;i<tab.length;i++) {
+			System.out.println();
+			for(int j=0;j<tab[i].length;j++) {
+				System.out.print(tab[i][j] + " ");
+			}
+		}
+	}
+	
+	
+	/* ======================================
+	 * Fonctions utilisées par l'IA numéro 3
+	 * ============== VVVVVV ================
+	 */
+	//(pas encore de fonctions)
+	//AI3 implémentera minMax avec la coupe Alpha / Beta
+	
+	
+}
